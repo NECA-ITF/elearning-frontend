8fa4e2ea977f1478a805beab7822cd4931af6814
Abdulrasheed <rashysmooth@yahoo.com>
2022-09-05 12:10:56 +0100

    chore: recent update on the button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CustomButton from './components/customButton/CustomButton';
 import CustomInput from './components/customInput/CustomInput';
+import './App.css';
 
 function App() {
   return (
