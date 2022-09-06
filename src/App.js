4e78d657d396f949c9924996e3b5e5d1a6d26ca3
Abdulsamad Rabo <raboabdulsamad04@gmail.com>
2022-09-06 15:39:50 +0100

    chore: first upload of page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import CustomButton from './components/customButton/CustomButton';
-import CustomInput from './components/customInput/CustomInput';
+// import CustomButton from './components/customButton/CustomButton';
+// import CustomInput from './components/customInput/CustomInput';
 import './App.css';
+// import ProfilePage from './pages/profilePage/ProfilePage';
+import SignupPage from './pages/signupPage/SignupPage';
 
 function App() {
   return (
     <div>
-        <CustomButton/>
-        <CustomInput/>
+        {/* <CustomButton/>
+        <CustomInput/> */}
+        <SignupPage />
     </div>
   )
 }
--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+// import img from '../../assets/image2.jpg';
+// import logo from '../../assets/itf_log.png';
+// import CustomInput from '../../components/customInput/CustomInput';
+// import CustomButton from '../../components/customButton/CustomButton';
 
 function LoginPage() {
   return (
