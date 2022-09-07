2d907b195aea1e59a390adf10ff93678da364024
Abdulrasheed <rashysmooth@yahoo.com>
2022-09-07 15:07:18 +0100

    chore: conflict resolved

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-// import CustomButton from './components/customButton/CustomButton';
-// import CustomInput from './components/customInput/CustomInput';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/homePage/HomePage';
+import SignupPage from './pages/signupPage/SignupPage';
+import LoginPage from './pages/loginPage/LoginPage';
+import MyCourses from './pages/myCourses/MyCourses';
 import './App.css';
 
 
 function App() {
   return (
     <div>
-        {/* <CustomButton/>
-        <CustomInput/> */
+      <Router>
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signupPage" element={<SignupPage />} />
+        <Route path="/loginPage" element={<LoginPage />} />
+        <Route path="myCourses/" element={<MyCourses />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
