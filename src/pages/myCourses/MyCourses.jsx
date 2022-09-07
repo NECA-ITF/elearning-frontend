4feb8510a5a1d2cb539cc16b0228b069e1fa0aef
unknown <khoinom2018.gmail.com>
2022-09-07 14:39:30 +0100

    chore:added rating stars and navbar

--- src/pages/myCourses/MyCourses.jsx
+++ src/pages/myCourses/MyCourses.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import video from './courseAssets/vid1.mp4'
-import CustomButton from '../../components/customButton/CustomButton'
+import thumbnails from '../myCourses/courseAssets/react-thumbnail.jpg'
+import '../../App.css'
 import './MyCourses.css'
-
+import NavBar from './NavBar'
+import Ratings from './Ratings'
 function MyCourses() {
   return (
-    <div className='course-container'>
+    <div>
+      <NavBar/>
+      <div className='course-container'>
       <div className="course-video">
           <div style={{ width: '100%'}}>
-            <video controls src={video} alt="video" id='course-vid' style={{ width: '100%'}}/>
+            <video controls src={video} alt="video" id='course-vid' style={{ width: '100%'}} poster={thumbnails}/>
           </div>
           <ul>
             <li>Course Materials</li>
@@ -17,8 +21,7 @@ function MyCourses() {
           </ul>
           <div className="course-section">
             <h1 className='course-header'>Introduction to <span>React.</span></h1><p className='course-p'> Learn everything about React, from the basics, to advanced topics like React components, props, hooks, among others</p>
-          </div>
-          <div className='course-reviews'>
+            <p className='rates'>Ratings <Ratings/></p>
           </div>
       </div>
       <div className="sub-outline">
@@ -37,6 +40,8 @@ function MyCourses() {
           <div className='course-list'>  State</div>
       </div>
     </div>
+    </div>
+    
   )
 }
 
