670de6b9b63f94a3ec2a79512d1575a2d927ed03
unknown <khoinom2018.gmail.com>
2022-09-08 15:25:49 +0100

    chore:resolved conflict and improved scrolling effect

--- src/pages/myCourses/MyCourses.jsx
+++ src/pages/myCourses/MyCourses.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import video from './courseAssets/vid1.mp4'
 import thumbnails from '../myCourses/courseAssets/react-thumbnail.jpg'
+import star from '../myCourses/courseAssets/rating.svg'
+import hover from '../myCourses/courseAssets/star.svg'
 import '../../App.css'
 import './MyCourses.css'
 import MyCourseNav from './MyCourseNav'
-import Ratings from './Ratings'
+import Ratings from 'react-rating'
 
 
 function MyCourses() {
@@ -23,11 +25,20 @@ function MyCourses() {
           </ul>
           <div className="course-section">
             <h1 className='course-header'>Introduction to <span>React.</span></h1><p className='course-p'> Learn everything about React, from the basics, to advanced topics like React components, props, hooks, among others.</p>
-            <p className='rates'>Ratings <Ratings/></p>
+            <p className='rates'>Ratings</p>
+            <Ratings
+            placeholderRating={3.5}
+            emptySymbol={<img src={star} className="icon" alt='#' style={{width:'23px'}} />}
+            placeholderSymbol={<img src={star} className="icon" alt='#' style={{width:'23px'}} />}
+            fullSymbol={<img src={hover} className="icon" alt='#' style={{width:'23px'}} />}
+            />
           </div>
       </div>
       <div className="sub-outline">
-          <h4 className='course-head'>Course Videos</h4>
+        <div className='course-head'>
+          <h4>Course Videos</h4>
+        </div>  
+          <div className='styky'></div>
           <div className='course-list'> Components</div>
           <div className='course-list'>  Hooks</div>
           <div className='course-list'>  Props</div>
@@ -40,6 +51,19 @@ function MyCourses() {
           <div className='course-list'>  Hooks</div>
           <div className='course-list'>  Props</div>
           <div className='course-list'>  State</div>
+          <div className='course-list'> Components</div>
+          <div className='course-list'>  Hooks</div>
+          <div className='course-list'>  Props</div>
+          <div className='course-list'>  State</div>
+          <div className='course-list'> Components</div>
+          <div className='course-list'>  Hooks</div>
+          <div className='course-list'>  Props</div>
+          <div className='course-list'>  State</div>
+          <div className='course-list'> Components</div>
+          <div className='course-list'>  Hooks</div>
+          <div className='course-list'>  Props</div>
+          <div className='course-list'>  State</div>
+          
       </div>
     </div>
     </div>
