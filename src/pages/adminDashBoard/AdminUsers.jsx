e134cd2b520529d599b3031ef158167d313984f5
unknown <ebenezeraluya@gmail.com>
2022-09-09 14:24:39 +0100

    chore: updated adminUsers

--- src/pages/adminDashBoard/AdminUsers.jsx
+++ src/pages/adminDashBoard/AdminUsers.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './AdminUsers.css'
 import CustomButton from '../../components/customButton/CustomButton'
+import gith from './adminAssets/gith.png'
+import linkd from './adminAssets/linkd.png'
+import ebenezer from './adminAssets/ebenezer.JPG'
+
 
 function adminUsers() {
   return (
@@ -12,8 +16,26 @@ function adminUsers() {
                     <span className='sectiontabs'>All Users</span>
                 </div>
             </div> 
-            
-            </div>
+
+        <div className='ab'>
+            <h2>All Users</h2>
+        {/* <div className='about'></div> */}
+        </div>
+
+        <div className='aboutpage'>
+                <div className='ebenezer'>
+                    <img src={ebenezer} alt="logo" width={250}/>
+                </div>
+                <div className='font'><h2>Aluya Ebenezer</h2></div>
+                <div className='socials'>
+                <img className='gitpix' src={gith} alt="g-icon" />
+                <img className='linkdpix' src={linkd} alt="l-icon"/>
+                </div>
+        </div>
+    </div>
+
+
+    
   )
 }
 
