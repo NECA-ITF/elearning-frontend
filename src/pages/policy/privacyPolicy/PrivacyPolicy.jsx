dfe3c203052831f190042493ace51abdf97a7d4d
Abdulrasheed <rashysmooth@yahoo.com>
2022-09-16 12:38:50 +0100

    feat: profile page adjustment

--- src/pages/policy/privacyPolicy/PrivacyPolicy.jsx
+++ src/pages/policy/privacyPolicy/PrivacyPolicy.jsx
@@ -7,22 +7,28 @@ function PrivacyPolicy() {
     return (
       <div className='abouthead'>
         <h1>PRIVACY POLICY</h1>
-        <div className='policy'><p>This Eduspot privacy policy collects, uses, consults or otherwise processes an individual's Personal Data. For the purposes of this policy, "ITF" refers to Eduspot, a Web Development Project incorporated into ITF-NECA program, having its courses at ITF House. In all cases described in this Policy, Eduspot will process your personal data as a data Controller.</p>
-<p>We are committed to protecting the privacy of our users and customers. This Privacy Policy is especially directed at:
-</p>    
-    <ul>
-    <li>visitors and subscribers of this e-learning platform;</li>
-	<li>natural persons contacting us through the contact or other forms available on the platform;</li>
-	<li> subscribers to our newsletter(s);</li>
-	<li>users signing in to use our services;</li>
-	<li>users signing in to download Free eBooks;</li>
-    <li> users signing in to attend and/or watch webinars;</li>
-    </ul>
-<p>This Privacy Policy is intended to inform you how we gather, define, and use information that could identify you, such as your name, email address, address, other contact details, online identifiers or other information that you provide to us when using our websites or when relying on our services. Please take a moment to read this Privacy Policy carefully.</p> 
-<p>Your personal data is created, stored and transmitted in a variety of paper and electronic formats, including databases at Eduspot. It is Eduspot’s policy to limit access to and the use of your personal data to ITF staff, faculty and authorized affiliates who have a legitimate interest in it for the purpose of carrying out their professional and contractual duties.</p>
-<p>Your personal data relating to your Eduspot-study program will be used, shared and otherwise processed by staff, faculty, and other persons directly associated with the program, as well as other ITF offices whose services support study abroad programs, including the Offices in the Federation.</p>
-<p>The program will also share your personal data with your school or prospective employer, and other Eduspot departments as part of your profile, and for a variety of other academic, educational, administrative, research and statistical purposes.
-</p></div>
+        <div className='policy'>
+          <p>This Eduspot privacy policy collects, uses, consults or otherwise processes an individual's Personal Data. For the purposes of this policy, "ITF" refers to Eduspot, a Web Development Project incorporated into ITF-NECA program, having its courses at ITF House. In all cases described in this Policy, Eduspot will process your personal data as a data Controller.
+          </p>
+          <p>We are committed to protecting the privacy of our users and customers. This Privacy Policy is especially directed at:
+          </p>
+          <ul>
+            <li>visitors and subscribers of this e-learning platform;</li>
+            <li>natural persons contacting us through the contact or other forms available on the platform;</li>
+            <li> subscribers to our newsletter(s);</li>
+            <li>users signing in to use our services;</li>
+            <li>users signing in to download Free eBooks;</li>
+            <li> users signing in to attend and/or watch webinars;</li>
+          </ul>
+          <p>This Privacy Policy is intended to inform you how we gather, define, and use information that could identify you, such as your name, email address, address, other contact details, online identifiers or other information that you provide to us when using our websites or when relying on our services. Please take a moment to read this Privacy Policy carefully.
+          </p> 
+          <p>Your personal data is created, stored and transmitted in a variety of paper and electronic formats, including databases at Eduspot. It is Eduspot's policy to limit access to and the use of your personal data to ITF staff, faculty and authorized affiliates who have a legitimate interest in it for the purpose of carrying out their professional and contractual duties.
+          </p>
+          <p>Your personal data relating to your Eduspot-study program will be used, shared and otherwise processed by staff, faculty, and other persons directly associated with the program, as well as other ITF offices whose services support study abroad programs, including the Offices in the Federation.
+          </p>
+          <p>The program will also share your personal data with your school or prospective employer, and other Eduspot departments as part of your profile, and for a variety of other academic, educational, administrative, research and statistical purposes.
+          </p>
+        </div>
       </div>
     )
   }
