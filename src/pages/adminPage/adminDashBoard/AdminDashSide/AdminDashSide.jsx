c4e5ce6f3979551d24bd78edffcd293712fd0b44
olaniyiisrael <davdto56@gmail.com>
2022-09-27 12:45:11 +0100

    chore:homepage routing in admin

--- src/pages/adminPage/adminDashBoard/AdminDashSide/AdminDashSide.jsx
+++ src/pages/adminPage/adminDashBoard/AdminDashSide/AdminDashSide.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import CustomButton from '../../../../components/customButton/CustomButton'
 import './AdminDashSide.css'
+import itflogo from '../../../../assets/itf_log.png';
 import { Link } from 'react-router-dom'
 
 function AdminDashSide() {
   return (
     <div className="adminDashSide">
         <div className="dashSideBtn">
+            <div style={{ width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Link to='/' className='links'><img src={itflogo} alt="itflogo" style={{width:'60px', height:'60px', padding:'5px 5px'}}/></Link>
+                <h4 style={{ color: "white" }}>Admin Dashboard</h4>
+            </div>
         <Link to='/admin-dash' className='links'>
             <CustomButton  
                 title="All Courses" 
