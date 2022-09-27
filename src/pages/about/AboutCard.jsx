5d798f67276070eb123fdded22af67b18e303941
Gabbie99 <khoinom2018@gmail.com>
2022-09-27 15:27:52 +0100

    chore:added on hover effect on about page

--- src/pages/about/AboutCard.jsx
+++ src/pages/about/AboutCard.jsx
@@ -1,19 +1,21 @@
-import { GitlabLogoSimple } from 'phosphor-react'
 import React from 'react'
 import './AboutPage.css'
 import gitlogo from './aboutAssets/gith.png'
 import { Link,} from 'react-router-dom';
 // import user from './users.json'
 
-function AboutCard({img, github, name}) {
+function AboutCard({img, github, name, about}) {
   return (
     <div>
-           <div className='cardpics'>
-               <img className='pics' src={img} alt="image" />
-                   <h3>{name}</h3>
-                <Link to={github}><img src={gitlogo} alt="" style={{width:"30px"}}/></Link> 
-          </div>
-
+        <div className='cardpics'>
+            <img className='pics' src={img} alt="image" />
+            <div class="service-hover-text">
+              <h3>Jane Doe</h3>
+              <p>{about}</p>
+            </div>
+                <h3>{name}</h3>               
+            <Link to={github}><img src={gitlogo} alt="" style={{width:"30px"}}/></Link> 
+        </div>
     </div>
   )
 }
