6dbb3bd2eaf0ed2731657f4058c724a62a87e623
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-09-30 15:00:00 +0100

    chore change the logout function

--- src/pages/signupPage/SignupPage.jsx
+++ src/pages/signupPage/SignupPage.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import img from '../../assets/image2.jpg'
 import logo from '../../assets/itf_log.png'
 import CustomInput from '../../components/customInput/CustomInput';
