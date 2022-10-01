0e8e987a43bd9ff62587ebfb5aade16817377ca1
Hameedarh <ajibadeolayimika@gmail.com>
2022-10-01 19:04:59 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -25,8 +25,8 @@ import CourseMaterials from './pages/courseMaterials/CourseMaterials'
 
 
 function App() {
-  // const API_URL = "http://localhost:5000";
-  const API_URL = "http://192.168.1.3:5000";
+  const API_URL = "http://localhost:5000";
+  // const API_URL = "http://192.168.1.3:5000";
 
   const [currentCourse, setCurrentCourse] = useState({});
   const [currentCourseOutline, setCurrentCourseOutline] = useState({});
