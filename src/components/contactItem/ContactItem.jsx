fc5912184d1cd1451c37da98cbad7378853be006
unknown <muelinx@gmail.com>
2022-10-04 14:23:55 +0100

    feat: worked on admin contact page

--- src/components/contactItem/ContactItem.jsx
+++ src/components/contactItem/ContactItem.jsx
@@ -3,22 +3,35 @@ import CustomButton from '../customButton/CustomButton';
 import './ContactItem.css'
 function ContactItem({ msg, index }) {
   return (
+    <div className='contact-rex'>
+       <div className='contact-list'>
+            <ul>
+              <li className='item'>No:</li>
+              <li className='item'>Name:</li>
+              <li className='item'>Phone Number:</li>
+              <li className='item'>E-mail:</li>
+              <li className='item'>Message:</li>
+              {/* <li className='item-sent'>Time Sent</li> */}
+            </ul>
+          </div>
+
     <div className='contact-details'>
-        <input type="checkbox" name="tick" id="tick" />
         <p className="serial-no item">{index}</p>
-        <div className='contact-name item' >
+        <div className='contact-name-item' >
              <p>{msg.name}</p>
         </div>
-        <div className='contact-number item'>
+        <div className='contact-number-item'>
             <p>{msg.phone}</p>
         </div>
-        <div className='contact-mail item'>{msg.email}</div>
-        <div className='contact-message item'> <p>{msg.message}</p> </div>
-        <div className="item">
+        <div className='contact-mail-item'>{msg.email}</div>
+        <div className='contact-message-item'> <p>{msg.message}</p> </div>
+        <div className='contact-time-item'> <p>Time Sent:--</p><p>{msg.createdAt}</p> </div>
+        {/* <div className="item">
           <CustomButton title='x' style={{backgroundColor:'#202c46', color:'white', height: '5vh', padding: '0px', boxShadow: 'none'}}/>
-        </div>
+        </div> */}
     </div>
     
+    </div>
   )
 }
 
