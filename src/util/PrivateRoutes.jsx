dfaf32ce0cfd9647470efc5ae018ce30f595242f
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-10-06 11:32:19 +0100

    chore:cleanup the useEffect in the waiting Page

--- src/util/PrivateRoutes.jsx
+++ src/util/PrivateRoutes.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Outlet,useNavigate } from 'react-router-dom'
+import { Outlet,Navigate } from 'react-router-dom'
 
 function PrivateRoutes() {
     //declare the auth
-    let auth = {token:false}
-    //ddeclare the navigate
-    const navigate = useNavigate()
+    let auth = {token:true}
     return (
-        auth.token ? <Outlet /> : navigate("/login")
+        auth.token ? <Outlet /> : <Navigate to="/login" />
     )
 }
 
